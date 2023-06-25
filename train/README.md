79d5a71e46b1b4c7c574a12d1203a38b5052532b
Racheal <81941550+Rachmtbs@users.noreply.github.com>
2023-06-25 12:34:04 -0700

    Add files via upload

--- train/README.md
+++ train/README.md
@@ -1,4 +1,5 @@
 # Train 
 
-A Pen created on CodePen.io. Original URL: [https://codepen.io/ract/pen/BaqRezd](https://codepen.io/ract/pen/BaqRezd).
+A Pen created on CodePen.io. Original URL: [https://codepen.io/ract/pen/mdzMqaK](https://codepen.io/ract/pen/mdzMqaK).
 
+I made this train to play with css elements.
---
+++ train/src/script.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function () {
+  let redCircle = document.getElementById("redCircle");
+  let aCircle = document.getElementById("aCircle");
+  let line = document.getElementById("line");
+  
+   redCircle.style.animationPlayState = 'running';
+   line.style.animationPlayState = 'running';
+    aCircle.style.animationPlayState = 'running';
+  });
+
